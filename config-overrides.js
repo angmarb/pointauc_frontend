e0a73c671f9bb27fd7362de9da6034577e3c3d85
artem <babkin.artem@gmail.com>
2023-04-30 20:52:32 +0500

    move to docs to trick pages

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   paths: function (paths) {
+    paths.appBuild = path.resolve(__dirname, './docs')
     paths.appPublic = path.resolve(__dirname, './src/assets');
     paths.appHtml = path.resolve(__dirname, './src/assets/index.html');
     return paths;
